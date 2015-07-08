55e880d71de81e16afbcc0e437e28c423dc6ffc7
Q3R-Solutions <Q3R-Solutions@192.168.1.37>
2015-07-08 02:32:20 -0500

    Nuevas modificaciones al proyecto

--- src/Controlador/CtrlScreenHabitaciones.java
+++ src/Controlador/CtrlScreenHabitaciones.java
@@ -3,7 +3,8 @@ package Controlador;
 import Logica.HabitacionLogica;
 import Modelo.Habitacion;
 import java.awt.BorderLayout;
-import java.util.Iterator;
+import java.awt.event.ActionEvent;
+import java.awt.event.ActionListener;
 import java.util.List;
 import javax.swing.JDesktopPane;
 import javax.swing.JPanel;
@@ -11,60 +12,29 @@ import javax.swing.JScrollPane;
 import vista.ScreenHabitaciones;
 import vista.itemHabitacion;
 
-public class CtrlScreenHabitaciones {
+public class CtrlScreenHabitaciones implements ActionListener{
 
     ScreenHabitaciones formScreen = new ScreenHabitaciones();
     JDesktopPane deskMain;
     List<Habitacion> listHab = null;
     HabitacionLogica habLog = null;
+    itemHabitacion item_1 = null;
+    JPanel content = new JPanel();
     
     public CtrlScreenHabitaciones(ScreenHabitaciones form, JDesktopPane panel) {
         this.formScreen = form;  
         this.deskMain = panel;
-        itemHabitacion item_1;  
+          
         habLog = new HabitacionLogica();
                 
         this.formScreen.setLayout(new BorderLayout());
         this.formScreen.setBounds(90,90, 850, 500);
-        
                 
-        JPanel content = new JPanel(); 
         content.setLayout(new ModifiedFlowLayout(ModifiedFlowLayout.LEFT, 20, 20));
         
-        
-        listHab = habLog.ListarHabitaciones_Item();
-
-        for(Habitacion hab : listHab){
-            item_1 = new itemHabitacion();
-            item_1.jButton2.setActionCommand(hab.getIdHabitacion()+"");
-            item_1.jLabel5.setText("HAB - "+hab.getNumero());//nombre habitacion
-            
-            String estado = "";
-            switch(hab.getEstado()){
-                case 1:
-                    estado = "LIBRE";
-                    break;
-                case 2:
-                    estado = "OCUPADO";                    
-                    break;
-                case 3:
-                    estado = "MANTENIMIENTO";
-                    break;
-                case 4:
-                    estado = "LIMPIEZA";
-                    break;
-                case 5:
-                    estado = "DESABILITADO";
-                    break;
-            }            
-            item_1.jLabel2.setText(estado);//estado
-            if(hab.getEstado()!= 1){
-                item_1.jButton2.setEnabled(false);
-            }
-            
-            
-            content.add(item_1);
-        }
+        //agregar items habitacion
+        generarItemsHabitacion(item_1, content);
+        //end
         
         JScrollPane scroll = new JScrollPane(content);        
         scroll.getVerticalScrollBar().setUnitIncrement(10);
@@ -74,10 +44,81 @@ public class CtrlScreenHabitaciones {
         
        
     }
-    
+    private void generarItemsHabitacion(itemHabitacion item_1, JPanel content){
+        
+        listHab = habLog.ListarHabitaciones_Item();
+
+        for(Habitacion hab : listHab){
+            item_1 = new itemHabitacion();
+            item_1.jButton2.setActionCommand(hab.getIdHabitacion()+"");
+            item_1.jLabel5.setText("HAB - "+hab.getNumero());//nombre habitacion
+            item_1.jButton2.setEnabled(false);///desabilito todos los botones
+            
+            
+            String estado = "";
+            String rutaImage = "";
+            switch(hab.getEstado()){
+                case 1:
+                    estado = "LIBRE";
+                    rutaImage = "/Imagenes/hab-libre.png";
+                    item_1.setBackground(new java.awt.Color(137, 183, 59));
+                    break;
+                case 2:
+                    estado = "OCUPADO";   
+                    rutaImage = "/Imagenes/hab-ocupada.png";
+                    item_1.setBackground(new java.awt.Color(204,51,0));
+                    break;
+                case 3:
+                    estado = "MANTENIMIENTO";
+                    rutaImage = "/Imagenes/hab-mantenimiento.png";
+                    item_1.setBackground(new java.awt.Color(0,102,153));
+                    break;
+                case 4:
+                    estado = "LIMPIEZA";
+                    rutaImage = "/Imagenes/hab-limpieza.png";
+                    item_1.setBackground(new java.awt.Color(255,204,0));
+                    break;
+                case 5:
+                    estado = "DESABILITADO";
+                    rutaImage = "/Imagenes/hab-deshabilitado.png";
+                    item_1.setBackground(new java.awt.Color(153,153,153));
+                    break;
+            }            
+            item_1.jLabel2.setText(estado);//estado
+            item_1.jLabel3.setIcon(new javax.swing.ImageIcon(getClass().getResource(rutaImage)));
+            if(hab.getEstado()== 2){
+                item_1.jButton2.setText("DESOCUPAR");
+            }
+            if(hab.getEstado()== 1){
+                item_1.jButton2.setEnabled(true);
+            }else if(hab.getEstado()== 2){
+                item_1.jButton2.setEnabled(true);
+            }
+            
+            content.add(item_1);
+        }
+        
+        
+        JButton btnRefresh = new JButton();
+        btnRefresh.setIcon(new javax.swing.ImageIcon(getClass().getResource("/Imagenes/refresh2.png")));
+        btnRefresh.setActionCommand("btnRefresh");
+        
+        content.add(btnRefresh);
+        btnRefresh.addActionListener(this);
+        
+    }
     public void showForm(){       
         deskMain.add(formScreen);
         this.formScreen.show();        
+    }
+
+    @Override
+    public void actionPerformed(ActionEvent e) {
+        if(e.getActionCommand().equals("btnRefresh")){
+            content.removeAll();
+            generarItemsHabitacion(item_1, content);
+            content.updateUI();
+        }
     }
    
     
